1587f85718b12d2d4cc64ba2268d434917cb5377
lil321 <lawrence.x.li.-nd@disney.com>
2020-11-19 10:11:52 +0800

    first commit

--- taobao-order/taobao-order-service/src/main/java/com/taobao/order/demo/Demo03.java
+++ taobao-order/taobao-order-service/src/main/java/com/taobao/order/demo/Demo03.java
@@ -1,7 +1,13 @@
 package com.taobao.order.demo;
 
+import lombok.var;
+
+import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 /**
  * @author Lawrence Li
@@ -24,14 +30,47 @@ public class Demo03 {
 //        });
 //        System.out.println(max);
 
-        conString("Lebron James", param -> param.toUpperCase(), param -> System.out.println(new StringBuilder(param).reverse().toString()));
+//        conString("Lebron James", param -> System.out.println(param.toUpperCase()), param -> System.out.println(new StringBuilder(param).reverse().toString()));
+
+        ArrayList<String> list = new ArrayList<>();
+        Stream<String> stream = list.stream();
+
+        String[] arr = {"hello ","java"};
+        Stream<String> arr1 = Stream.of(arr);
+
+
+
+    }
+
+    private static void convert(String name, Function<String, Integer> fun){
+        Integer apply = fun.apply(name);
+        System.out.println(apply);
+    }
+
+
+    private boolean checkString(String name, Predicate<String> pre1, Predicate<String> pre2){
+       return pre1.and(pre2).test(name);
+    }
+
+    private boolean checkString(String name, Predicate<String> pre){
+        boolean boo = pre.test(name);
+        return boo;
+    }
+
+    private void printInfo(String[] strArray, Consumer<String> con1, Consumer<String> con2){
+
+        for (String s : strArray) {
+            con1.andThen(con2).accept(s);
+        }
     }
 
 
 
     public static void conString(String name, Consumer<String> consumer1, Consumer<String> consumer2){
-        consumer1.accept(name);
-        consumer2.accept(name);
+//        consumer1.accept(name);
+//        consumer2.accept(name);
+        consumer1.andThen(consumer2).accept(name);
+
     }
 
 
